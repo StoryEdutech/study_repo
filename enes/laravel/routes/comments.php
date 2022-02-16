244681490d50cb6a5c6c6fe18165e0328220b766
enes1004 <karabacakenes@gmail.com>
2022-02-17 07:41:10 +0900

    add route

--- enes/laravel/routes/comments.php
+++ enes/laravel/routes/comments.php
@@ -7,4 +7,4 @@ use App\Http\Controllers\CommentController;
 Route::resource('/comment', CommentController::class)->middleware(['auth','child_auth']);
 Route::get('/comment/{id}/delete',[CommentController::class,'confirm_delete'])->middleware(['auth','child_auth']);
 Route::get('/comment/{id}/reply_to',[CommentController::class,'reply_to'])->middleware(['auth','child_auth']);
-Route::get('/comment/user/{uid}',[CommentController::class,'index'])->middleware(['auth','child_auth']);
+Route::get('/comment/user/{uid}',[CommentController::class,'view_index'])->middleware(['auth','child_auth']);
