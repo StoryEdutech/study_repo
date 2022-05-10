b5f8fd46dc41d38b8ec13ca1eb88bb312dd186b4
enes1004 <karabacakenes@gmail.com>
2022-05-11 00:17:20 +0900

    test again

---
+++ enes/laravel/config/kobetsuba_api_client.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+  'client_id'=>env('KOBETSUBA_OAUTH_CLIENT','96442e58-076e-440a-bc69-2f8ad585e8da'),
+  'client_secret'=>env('KOBETSUBA_OAUTH_SECRET','m17LZF3R1mOUYVWvZZ1Vkr5veNiGH642fOu0DM0i'),
+  'url'=>env('KOBETSUBA_API_URL','https://chugakujyuken.kobetsuba.jp/apis')
+];
