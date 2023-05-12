1782841b7d6fe2acc079e56b0199d1ca69de561e
武井優樹 <hukupon0528@gmail.com>
2023-05-12 14:13:01 +0900

    styled-components button––

--- takei/blog/resources/js/Pages/Post/Index.jsx
+++ takei/blog/resources/js/Pages/Post/Index.jsx
@@ -1,55 +1,76 @@
 import React from "react";
-// import Authenticated from "@/Layouts/Authenticated";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+
+import { Head,Link } from '@inertiajs/react';
+import styled from "styled-components"
+
+const Button = styled.button`
+  display: inline-block;
+  color: #333;
+  border: 2px solid #333;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+  display: block;
+`;
+
+const RegistButton = styled(Button)`
+  background: #FBF4D6;
+  border-width: 1px;
+`;
+const DeleteButton = styled(Button)`
+  color: #DB5461;
+  border-color: #DB5461;
+`;
+
 
 export default function Index(props) {
-    console.log('props',props);
+    console.log('props', props);
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Blog
+                    ブログ
                 </h2>
             }
         >
             <Head title="Blog Index" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            Blog Index
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <div className="p-6 bg-white border-b border-gray-200">
+                <div>
+                    <Link href={route("post.create")}>
+                        <RegistButton>新規作成</RegistButton>
+                    </Link>
+                </div>
                 <table>
-                    <thead>
-                        <tr>
-                            <th>タイトル</th>
-                            <th>コンテンツ</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.posts?.map((post) => {
-                            return (
-                                <tr key={post.id}>
-                                    <td className="border px-4 py-2">
-                                        {post.title}
-                                    </td>
-                                    <td className="border px-4 py-2">
-                                        {post.content}
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+    <thead>
+        <tr>
+            <th>タイトル</th>
+            <th>コンテンツ</th>
+            <th>削除</th>
+        </tr>
+    </thead>
+    <tbody>
+        {props.posts.map((post) => {
+            return (
+                <tr key={post.id}>
+                    <td className="border px-4 py-2">
+                        {post.title}
+                    </td>
+                    <td className="border px-4 py-2">
+                        {post.content}
+                    </td>
+                    <td className="border px-4 py-2">
+                        <DeleteButton>削除</DeleteButton>
+                    </td>
+                </tr>
+            );
+        })}
+    </tbody>
+</table>
             </div>
         </AuthenticatedLayout>
     );
