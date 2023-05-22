6c7e8c4daf572fdc096073186bf7652cd2f994ef
武井優樹 <hukupon0528@gmail.com>
2023-05-22 21:30:52 +0900

    fix design

--- takei/blog/resources/js/Pages/Posts/Show.jsx
+++ takei/blog/resources/js/Pages/Posts/Show.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 
 import { Head } from '@inertiajs/react';
+import styled from "styled-components"
+
+const UserName = styled.div`
+font-size: 1.4rem;
+margin-bottom: 2rem;
+`;
+const PostTitle = styled.div`
+font-size: 1.4rem;
+`;
+const PostContent = styled.div`
+margin-top: 3rem;
+`;
 
 export default function Show(props) {
-    const {post} = props;
-    const {title, content} = post;
+    const { post } = props;
+    const { title, content } = post;
     return (
         <AuthenticatedLayout
             auth={props.auth}
@@ -20,24 +32,14 @@ export default function Show(props) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            {/* <ValidationErrors errors={errors} /> */}
-                            <div className="m-28">
-                                <div className="text-xl mb-6">タイトル</div>
-                                <div>
-                                    {title}
-                                </div>
-                            </div>
-                            <div className="m-28">
-                                <div className="text-xl mb-6">内容</div>
-                                <div>
-                                    {content}
-                                </div>
-                            </div>
-
-                            <div className="flex items-center justify-end mt-4">
-
-                            </div>
+                        <div className="p-6 bg-white border-b border-gray-200 m-28">
+                            <UserName>
+                                投稿者：{post?.user?.name || ""}
+                            </UserName>
+                            <PostTitle>{title}</PostTitle>
+                            <PostContent>
+                                {content}
+                            </PostContent>
                         </div>
                     </div>
                 </div>
