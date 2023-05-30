d0af37265789a7c2dab41adfd6fd3718b0528d82
武井優樹 <hukupon0528@gmail.com>
2023-05-30 15:58:09 +0900

    ファイル名の変更 それに伴うInertia::renderの変更

--- takei/blog/app/Http/Controllers/PostController.php
+++ takei/blog/app/Http/Controllers/PostController.php
@@ -31,7 +31,7 @@ class PostController extends Controller
     public function create()
     {
         //
-        return Inertia::render('Posts/Create');
+        return Inertia::render('Posts/PostCreatePage');
     }
 
     /**
@@ -58,7 +58,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return Inertia::render('Posts/Show',['post' => $post]);
+        return Inertia::render('Posts/PostShowPage',['post' => $post]);
     }
 
     /**
@@ -69,7 +69,7 @@ class PostController extends Controller
      */
     public function edit(Request $request, Post $post)
     {
-        return Inertia::render('Posts/Edit',['post' => $post]);
+        return Inertia::render('Posts/PostEditPage',['post' => $post]);
     }
 
     /**
