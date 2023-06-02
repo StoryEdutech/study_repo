f1a3c46f50296623f11cc0c1c589e2ba5ce3614e
武井優樹 <hukupon0528@gmail.com>
2023-06-02 14:24:16 +0900

    コードの改善

--- takei/blog/app/Http/Controllers/PostController.php
+++ takei/blog/app/Http/Controllers/PostController.php
@@ -19,7 +19,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Posts/List',['posts' => Post::with('user')->get()]);
+        return Inertia::render('Posts/List',[
+            'posts' => Post::with('user')->get()
+        ]);
     }
 
     /**
