cb983b65910cdf6b7d08df1cc4c07bc49828c9cb
zvvlj0219 <s00302448@gmail.com>
2023-06-08 19:12:10 +0900

    インポートの仕方を修正

--- kinoshita/next-13-tutorial/blog-app/lib/components/index.tsx
+++ kinoshita/next-13-tutorial/blog-app/lib/components/index.tsx
@@ -1,13 +1,4 @@
 "use client";
 
 // ファイルのサイズが大きいので、１つ１つ名前付きインポートして、エクスポートする
-// export * from "@chakra-ui/react";
-
-export {
-    Box,
-    Flex,
-    Heading,
-    Button,
-    Container,
-    Text
-} from "@chakra-ui/react";
+export * from "@chakra-ui/react";
