09e7de6f64c4141c4ec506d257e9cbb650f8b373
zvvlj0219 <s00302448@gmail.com>
2023-06-14 13:37:17 +0900

    記事一覧 api route修正

--- kinoshita/next-13-tutorial/blog-app/app/api/articles/route.ts
+++ kinoshita/next-13-tutorial/blog-app/app/api/articles/route.ts
@@ -1,25 +1,6 @@
 import { NextResponse } from 'next/server'
-import type { Article } from '@/app/types'
-
-const sampleArticles: Article[] = [
-    {
-        id: 1,
-        title: "タイトル1",
-        content: "ここが本文",
-        slug: "slug_1",
-        createdAt: new Date(),
-        updatedAt: null
-    },
-    {
-        id: 2,
-        title: "タイトル2!!!!!!",
-        content: "ここが本文",
-        slug: "slug_2",
-        createdAt: new Date(),
-        updatedAt: null
-    },
-]
+import articlesData from '@/dummy-data/articles.json'
 
 export async function GET() {
-    return NextResponse.json({ articles: sampleArticles})
+    return NextResponse.json({ articles: articlesData})
 }
