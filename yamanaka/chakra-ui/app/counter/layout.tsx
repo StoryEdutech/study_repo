a6b75acb1cbeb39f462e0b12ced17463666431ca
yamanga <131602758+yamanga@users.noreply.github.com>
2023-11-07 17:57:41 +0900

    add header

--- yamanaka/chakra-ui/app/counter/layout.tsx
+++ yamanaka/chakra-ui/app/counter/layout.tsx
@@ -1,9 +1,15 @@
 import { Metadata } from 'next'
+import Header from '@/app/_components/Header'
 
 export const metadata: Metadata = {
   title: 'カウンター',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+  return (
+    <>
+      <Header title='カウンター' />
+      {children}
+    </>
+  )
 }
