9fbc3bc8ea6e5e04b88ff2e8faedd170ca706944
zvvlj0219 <s00302448@gmail.com>
2024-01-21 18:15:07 +0900

    post seed

--- kinoshita/practice-erd/instagram/laravel/database/seeders/DatabaseSeeder.php
+++ kinoshita/practice-erd/instagram/laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,11 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(5)->create()->each(function ($user) {
+            $user->posts()->saveMany(Post::factory(2)->make());
+        });
     }
 }
