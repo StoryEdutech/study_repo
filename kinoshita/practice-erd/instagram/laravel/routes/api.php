1562d9ebd7bd4773539cff6295e0c8efab49bf53
zvvlj0219 <s00302448@gmail.com>
2024-01-21 19:06:34 +0900

    route apiResource

--- kinoshita/practice-erd/instagram/laravel/routes/api.php
+++ kinoshita/practice-erd/instagram/laravel/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/post', PostController::class);
