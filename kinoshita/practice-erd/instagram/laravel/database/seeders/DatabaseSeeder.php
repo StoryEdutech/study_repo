2a0f4dd20c3d5f0c355be70592226e403b5bba09
zvvlj0219 <s00302448@gmail.com>
2024-01-21 18:46:23 +0900

    投稿のデータ

--- kinoshita/practice-erd/instagram/laravel/database/seeders/DatabaseSeeder.php
+++ kinoshita/practice-erd/instagram/laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -16,7 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(5)->create()->each(function ($user) {
-            $user->posts()->saveMany(Post::factory(2)->make());
+            $posts = Post::factory(2)->make(); // 投稿を生成
+
+            $user->posts()->saveMany($posts); // ユーザーに投稿を関連付け
+        
+            // 投稿に関連するコメントを生成
+            $posts->each(function ($post) use ($user) {
+                $comments = Comment::factory(3)->make(['user_id' => $user->id]);
+                $post->comments()->saveMany($comments);
+            });
         });
     }
 }
