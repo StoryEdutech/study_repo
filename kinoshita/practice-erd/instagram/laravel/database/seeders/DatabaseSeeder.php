60785c13c66b4266e82f6ca21df27e5ba5beeae8
zvvlj0219 <s00302448@gmail.com>
2024-02-04 23:00:40 +0900

    factory修正

--- kinoshita/practice-erd/instagram/laravel/database/seeders/DatabaseSeeder.php
+++ kinoshita/practice-erd/instagram/laravel/database/seeders/DatabaseSeeder.php
@@ -4,10 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Comment;
-use App\Models\Like;
 use App\Models\Post;
-use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -18,24 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create()->each(function ($user) {
-            $posts = Post::factory(2)->create(); // 投稿を生成
-        
-            $posts->each(function ($post) use ($user) {
-                // コメントのデータを紐づける
-                $comments = Comment::factory(3)->make(['user_id' => $user->id]);
-                $post->comments()->saveMany($comments);
-
-                // タグのデータを紐づける
-                $tags = Tag::factory(2)->create(['body' => fake()->word()]);
-                $post->tags()->attach($tags->pluck('id'));
-
-                //いいね
-                $likes = Like::factory(2)->make(['user_id' => $user->id]);
-                $post->likes()->saveMany($likes);
-            });
-
-            $user->posts()->saveMany($posts); // ユーザーに投稿を関連付け
-        });
+        User::factory(5) // ユーザー五人
+            ->has( 
+            Post::factory(2) // 全員に２投稿ずつあり
+                ->hasComments(3) // 各投稿にコメント3個ある
+                ->hasLikes(2) // 各投稿にいいね2個ある
+                ->hasTags(2) // 各投稿にたぐ2個ある
+            )->create();
     }
 }
