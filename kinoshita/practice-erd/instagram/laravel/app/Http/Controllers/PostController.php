7304484bdb34a0ecf3cde7fbe2fbff7768fd4b05
zvvlj0219 <s00302448@gmail.com>
2024-02-16 20:12:56 +0900

    投稿を編集 削除

--- kinoshita/practice-erd/instagram/laravel/app/Http/Controllers/PostController.php
+++ kinoshita/practice-erd/instagram/laravel/app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\Cast\String_;
 
 class PostController extends Controller
 {
@@ -27,8 +28,10 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(String $id)
     {
+        $post = Post::findOrFail($id); // なかったら404を自動で返してくれる
+
         $post->load([
             'comments' => [ 'likes' ], //Postコメントとそれぞれのコメントのいいね
             'likes', // Postのいいね
@@ -41,16 +44,24 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $new_content = $request->input('content');
+
+        $post->content = $new_content;
+
+        $post->save();
+
+        return response()->json(new PostResource($post));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        //
+       $post->delete();
+
+        return response(200, null);
     }
 }
