c7ee54bf481a3a5eede40661f241ba6722f709b6
SakumaMomoka <m.sakuma65@gmail.com>
2024-03-07 10:46:50 +0900

    fix

--- sakuma/mui/src/Study.js
+++ sakuma/mui/src/Study.js
@@ -1,49 +1,50 @@
 import { useState } from 'react';
-import { Button, Checkbox, FormControlLabel } from "@mui/material";
+import { Button } from "@mui/material";
 import { styled } from '@mui/system';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown, faArrowLeft, faArrowRight, faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
-const DivFlex = styled('div')({
-    display: 'flex',
-});
+const DivFlex = styled('div')`
+    display: flex;
 
-const MyLabel = styled(FormControlLabel)( ({checked}) => ({
+    ${({justify}) => justify ? `justify-content: ${justify}`:'' }
+`;
+
+const FlexTable = styled(DivFlex)`
+    max-width: 50px;
+    flex-direction: column;
+`;
+
+const MyToggleButton = styled('button')( ({clicked}) => ({
     display: 'block',
-    background: checked ? '#1a720d' :'#c8eac3',
+    background: clicked ? '#1a720d' :'#c8eac3',
     padding: '30px',
     borderRadius: '30px'
 }) );
 
-const MyCheckbox = styled(Checkbox)`
-    display:none
-`;
-
 export default function Study() {
-    const [checked, setChecked] = useState(false);
+    const [clicked, setClicked] = useState(false);
     return (
         <>
             <Button variant="text">Text</Button>
             <Button variant="contained">Contained</Button>
             <Button variant="outlined">Outlined</Button>
-            <DivFlex sx={{ maxWidth: '50px', flexFlow: 'column' }}>
-                <DivFlex sx={{ justifyContent: 'center'}}>
+            <FlexTable>
+                <DivFlex justify='center'>
                     <FontAwesomeIcon icon={faArrowUp} />
                 </DivFlex>
-                <DivFlex sx={{ justifyContent: 'space-between'}}>
+                <DivFlex justify='space-between'>
                     <FontAwesomeIcon icon={faArrowLeft} />
                     <FontAwesomeIcon icon={faArrowRight} />
                 </DivFlex>
-                <DivFlex sx={{ justifyContent: 'center'}}>
+                <DivFlex justify='center'>
                     <FontAwesomeIcon icon={faArrowDown} />
                 </DivFlex>
-            </DivFlex>
+            </FlexTable>
             <DivFlex>
-                <MyLabel 
-                    control={<MyCheckbox checked={checked} onChange={(e) => setChecked(e.target.checked)} />} 
-                    checked={checked} 
-                    label="トグルボタン" 
-                />
+                <MyToggleButton clicked={clicked} onClick={() => setClicked(!clicked)}>
+                    トグルボタン
+                </MyToggleButton>
             </DivFlex>
         </>
     );
